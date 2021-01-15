dc25e95b360e051018a4576156907541305df1ad
Anton Mamakin <antonmamakinscript@gmail.com>
2021-01-15 09:18:17 +0400

    hot fix

--- src/main/java/com/example/testjob/controller/MailingController.java
+++ src/main/java/com/example/testjob/controller/MailingController.java
@@ -4,6 +4,7 @@ import com.example.testjob.baseModel.*;
 import com.example.testjob.model.Mailing;
 import com.example.testjob.service.MailingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
